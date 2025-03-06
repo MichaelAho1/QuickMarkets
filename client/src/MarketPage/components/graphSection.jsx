96bb53266b1bd6138fbe174904167f1e92e5bea5
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-03-06 15:27:11 -0500

    Fixed GraphSections header and added styling

--- client/src/MarketPage/components/graphSection.jsx
+++ client/src/MarketPage/components/graphSection.jsx
@@ -3,14 +3,10 @@ import styles from './graphSection.module.css';
 
 const graphSection = () => {
     return (
-        <>
-        <div className={styles.header}>
-            <h1 className={styles.title}>Top50 <span className={styles.price}>$7,352.05</span></h1>
+        <div className={styles.graphSection}>
+            <h2 className={styles.title}>Top501 <span className={styles.price}>$7,352.05</span></h2>
+            <h3 className={styles.titleDescription}><span className={styles.changeInYear}>+105.55%</span> Past Year</h3>
         </div>
-        <div className={styles.headerDescription}>
-            <h3><span className={styles.yearTotal}>+105.55</span>% Past Year</h3>
-        </div>
-        </>
     );
 };
 
