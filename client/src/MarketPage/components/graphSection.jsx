7a6e7f4faceeb8a77b711ec38a1bf2a77d92d9e0
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-03-06 17:19:01 -0500

    added icon and title, adjusted styling for marketsection

--- client/src/MarketPage/components/graphSection.jsx
+++ client/src/MarketPage/components/graphSection.jsx
@@ -4,8 +4,13 @@ import styles from './graphSection.module.css';
 const graphSection = () => {
     return (
         <div className={styles.graphSection}>
-            <h2 className={styles.title}>Top501 <span className={styles.price}>$7,352.05</span></h2>
-            <h3 className={styles.titleDescription}><span className={styles.changeInYear}>+105.55%</span> Past Year</h3>
+            <div className={styles.graphHeader}>
+                <h2 className={styles.title}>Top50 <span className={styles.price}>$7,352.05</span></h2>
+                <h3 className={styles.titleDescription}><span className={styles.changeInYear}>+105.55%</span> | Past Year</h3>
+            </div>
+            <div className={styles.graph}>
+                
+            </div>
         </div>
     );
 };
