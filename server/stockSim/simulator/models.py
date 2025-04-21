17d88809d5b778945511c94a767c7cdd5e52f3e5
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-04-21 14:29:27 -0400

    Added Database Table and 10 sample stocks

---
+++ server/stockSim/simulator/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length=4)
+    name = models.CharField(max_length=25)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    volatility = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
+    avgReturn = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ticker
