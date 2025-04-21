277894d08f3d2809cbe29196d5a0dcdf09804395
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-04-21 19:31:58 -0400

    Made Sure Price Generator will generate a realistic return over 252 trading days

--- server/stockSim/simulator/stockGeneration/priceGenerator.py
+++ server/stockSim/simulator/stockGeneration/priceGenerator.py
@@ -4,46 +4,69 @@ from decimal import Decimal
 import math, random
 
 def generateNewPrices():
-    totalMarketChange = generateTotalMarketPrice("ALL")
+    marketChange = generateTotalMarketPrice("ALL")
     ETFChanges = {}
     stockChanges = {}
 
     ETFs = ETF.objects.all()
     for etf in ETFs:
         if(etf.ticker != "ALL"): #Exclude all because its price has already been calculated
-            ETFChanges[etf.ticker] = generateNewETFPrice(etf.ticker, totalMarketChange)
+            ETFChanges[etf.ticker] = generateNewETFPrice(etf.ticker, marketChange)
     
     stocks = Stock.objects.all()
     for stock in stocks:
         ETFTicker = stock.industry #Stocks industry are the same as the ETFs ticker symbol
         industryChange = ETFChanges[ETFTicker]
-        stockChanges[stock.ticker] = generateNewStockPrice(stock.ticker, totalMarketChange, industryChange)
-    
-    return stockChanges
+        stockChanges[stock.ticker] = generateNewStockPrice(stock.ticker, marketChange, industryChange)
+
+    total = 0
+    tentotal = 0
+    print(marketChange)
+    print(ETFChanges["TECH"])
+    print(stockChanges["NVDA"])
+    for i in range(0, 365):
+        total += generateTotalMarketPrice("ALL")
+    for i in range(0, 3650):
+        tentotal += generateTotalMarketPrice("ALL")
+    print(total)
+    print(tentotal)
 
 def generateTotalMarketPrice(ticker):
     TotalMarketETF = getETFData(ticker)
     changeInPrice = calculateGBM(float(TotalMarketETF.price), float(TotalMarketETF.volatility), float(TotalMarketETF.avgReturn))
     return changeInPrice
 
-def generateNewETFPrice(ticker, allChange):
+
+def generateNewETFPrice(ticker, MarketChange):
+    marketWeight = 0.75
+    randomWeight = 0.25
+
     ETF = getETFData(ticker)
-    changeInPrice = calculateGBM(float(ETF.price), float(ETF.volatility), float(ETF.avgReturn))
-    return changeInPrice
+    randomChange = calculateGBM(float(ETF.price), float(ETF.volatility), float(ETF.avgReturn))
+    #print(randomChange)
+    return (randomChange * randomWeight) + (MarketChange * marketWeight)
+
 
 def generateNewStockPrice(ticker, allChange, industryChange):
+    industryWeight = 0.6
+    randomWeight = 0.4
+
     stock = getStockData(ticker)
-    changeInPrice = calculateGBM(float(stock.price), float(stock.volatility), float(stock.avgReturn))
-    return changeInPrice
+    randomChange = calculateGBM(float(stock.price), float(stock.volatility), float(stock.avgReturn))
+    return (randomChange * randomWeight) + (industryChange * industryWeight)
 
+
+#Accounts for Volatility and Long-Term Growth in the market
 def calculateGBM(previousPrice, volatility, avgReturn):
-    #Convert Percentages into decimals
-    volatility = (volatility/100)/252
-    avgReturn = (avgReturn/100)/252
+    # Convert to decimal
+    volatility = volatility / 75
+    avgReturn = avgReturn / 200
+    dt = 1 / 252
 
-    #GBM formula
+    # GBM components
     epsilon = random.gauss(0, 1)
-    drift = avgReturn - 0.5 * volatility ** 2
-    shock = epsilon * volatility * math.sqrt(1)
+    drift = (avgReturn - 0.5 * volatility ** 2) * dt
+    shock = volatility * math.sqrt(dt) * epsilon
 
-    return (math.exp(drift + shock) - 1)*1000
+    # Return percent change
+    return (math.exp(drift + shock) - 1) * 100
