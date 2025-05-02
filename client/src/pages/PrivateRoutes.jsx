4fc41010cfa724b6b35f9490ed44643f3d75f871
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-02 15:56:01 -0400

    redid file structure

--- client/src/pages/PrivateRoutes.jsx
+++ client/src/pages/PrivateRoutes.jsx
@@ -2,15 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./PrivatePages/dashboard/dashboard.jsx";
 import ExploreStocks from "./PrivatePages/exploreStocks/exploreStocks.jsx";
 import Settings from "./PrivatePages/settings/settings.jsx";
-import StocksOwned from "./PrivatePages/stocksOwned/stocksOwned.jsx";
+import Portfolio from "./PrivatePages/portfolio/portfolio.jsx";
 
 export default function PrivateRoutes() {
   return (
     <Routes>
       <Route path="/admin" element={<Dashboard />} />
+      <Route path="/admin/login" element={<Portfolio />} />
       <Route path="/admin/users" element={<ExploreStocks />} />
       <Route path="/admin/stocks" element={<Settings />} />
-      <Route path="/admin/login" element={<StocksOwned />} />
     </Routes>
   );
 }
