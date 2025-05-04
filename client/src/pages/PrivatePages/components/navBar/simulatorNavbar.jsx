52dc3c305aac7e78100041ffe962f90df309b222
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-04 15:19:40 -0400

    added hamburger menu for the navbar for smaller screens

--- client/src/pages/PrivatePages/components/navBar/simulatorNavbar.jsx
+++ client/src/pages/PrivatePages/components/navBar/simulatorNavbar.jsx
@@ -6,16 +6,20 @@ import { MdOutlineDashboard } from "react-icons/md";
 import { BsPiggyBank } from "react-icons/bs";
 import { AiOutlineStock } from "react-icons/ai";
 import { IoSettingsOutline } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io"; // Caret icon
+import { IoIosArrowDown, IoMdMenu } from "react-icons/io"; // Added menu icon
 
 const Navbar = () => {
     const [isWatchlistCollapsed, setIsWatchlistCollapsed] = useState(false);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     const toggleWatchlist = () => {
         setIsWatchlistCollapsed(!isWatchlistCollapsed);
     };
 
-    // Simulated stock data with day changes (as of Saturday, May 3, 2025)
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     const stocks = [
         { symbol: 'AAPL', dayChangePercentage: 1.25 },
         { symbol: 'MSFT', dayChangePercentage: -0.78 },
@@ -25,62 +29,68 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className={styles.navBar}>
-            <div className={styles.navbarHeader}>
-                <img src={logo} alt="QuickMarkets Logo"></img>
-                <h1 className={styles.headerName}>
-                    <Link to="/">QuickMarkets</Link>
-                </h1>
-            </div>
-            <div className={styles.navBarContent}>
-                <div className={styles.mainNavLinks}>
-                    <div className={styles.mainNavTop}>
-                        <p className={styles.navBarHeader}>MAIN MENU</p>
-                        <p className={styles.navBarItem}>
-                            <MdOutlineDashboard />
-                            <Link to="/admin">Dashboard</Link>
-                        </p>
-                        <p className={styles.navBarItem}>
-                            <BsPiggyBank />
-                            <Link to="/admin/portfolio">Portfolio</Link>
-                        </p>
-                        <p className={styles.navBarItem}>
-                            <AiOutlineStock />
-                            <Link to="/admin/exploreStocks">Stocks</Link>
-                        </p>
+        <>
+            <button className={styles.hamburgerButton} onClick={toggleSidebar}>
+                <IoMdMenu className={styles.hamburgerIcon} />
+            </button>
+
+            <nav className={`${styles.navBar} ${isSidebarOpen ? styles.open : ''}`}>
+                <div className={styles.navbarHeader}>
+                    <img src={logo} alt="QuickMarkets Logo" />
+                    <h1 className={styles.headerName}>
+                        <Link to="/">QuickMarkets</Link>
+                    </h1>
+                </div>
+                <div className={styles.navBarContent}>
+                    <div className={styles.mainNavLinks}>
+                        <div className={styles.mainNavTop}>
+                            <p className={styles.navBarHeader}>MAIN MENU</p>
+                            <p className={styles.navBarItem}>
+                                <MdOutlineDashboard />
+                                <Link to="/admin">Dashboard</Link>
+                            </p>
+                            <p className={styles.navBarItem}>
+                                <BsPiggyBank />
+                                <Link to="/admin/portfolio">Portfolio</Link>
+                            </p>
+                            <p className={styles.navBarItem}>
+                                <AiOutlineStock />
+                                <Link to="/admin/exploreStocks">Stocks</Link>
+                            </p>
+                        </div>
+                        <div className={styles.mainNavBottom}>
+                            <p className={styles.navBarItem}>
+                                <IoSettingsOutline />
+                                <Link to="/admin/settings">Settings</Link>
+                            </p>
+                        </div>
                     </div>
-                    <div className={styles.mainNavBottom}>
-                        <p className={styles.navBarItem}>
-                            <IoSettingsOutline />
-                            <Link to="/admin/settings">Settings</Link>
+                    <div className={styles.watchList}>
+                        <p className={styles.navBarHeader} onClick={toggleWatchlist}>
+                            <span className={styles.watchListTitle}>WATCHLIST</span>
+                            <IoIosArrowDown className={`${styles.caret} ${isWatchlistCollapsed ? styles.collapsed : ''}`} />
                         </p>
+                        <div className={`${styles.collapsibleContent} ${isWatchlistCollapsed ? styles.collapsed : ''}`}>
+                            {stocks.map((stock) => (
+                                <div className={styles.watchListItem} key={stock.symbol}>
+                                    <span>{stock.symbol}</span>
+                                    <span className={stock.dayChangePercentage >= 0 ? styles.positiveChange : styles.negativeChange}>
+                                        {stock.dayChangePercentage >= 0 ? '+' : ''}{stock.dayChangePercentage.toFixed(2)}%
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-                <div className={styles.watchList}>
-                    <p className={styles.navBarHeader} onClick={toggleWatchlist}>
-                        <span className={styles.watchListTitle}>WATCHLIST</span>
-                        <IoIosArrowDown className={`${styles.caret} ${isWatchlistCollapsed ? styles.collapsed : ''}`} />
-                    </p>
-                    <div className={`${styles.collapsibleContent} ${isWatchlistCollapsed ? styles.collapsed : ''}`}>
-                        {stocks.map((stock) => (
-                            <div className={styles.watchListItem} key={stock.symbol}>
-                                <span>{stock.symbol}</span>
-                                <span className={stock.dayChangePercentage >= 0 ? styles.positiveChange : styles.negativeChange}>
-                                    {stock.dayChangePercentage >= 0 ? '+' : ''}{stock.dayChangePercentage.toFixed(2)}%
-                                </span>
-                            </div>
-                        ))}
+                <div className={styles.userProfile}>
+                    <img className={styles.userImg} src="" alt="User" />
+                    <div>
+                        <p className={styles.userName}>John Doe</p>
+                        <p className={styles.userEmail}>john.doe@example.com</p>
                     </div>
                 </div>
-            </div>
-            <div className={styles.userProfile}>
-                <img className={styles.userImg} src="" alt="User" />
-                <div>
-                    <p className={styles.userName}>John Doe</p>
-                    <p className={styles.userEmail}>john.doe@example.com</p>
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     );
 };
 
