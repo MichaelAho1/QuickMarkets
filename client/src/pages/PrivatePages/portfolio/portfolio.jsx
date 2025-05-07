9d89b3008e7e179c2286c2c5a28581099b16e756
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-07 00:09:44 -0400

    .

--- client/src/pages/PrivatePages/portfolio/portfolio.jsx
+++ client/src/pages/PrivatePages/portfolio/portfolio.jsx
@@ -3,36 +3,16 @@ import Navbar from "../components/navBar/simulatorNavbar.jsx";
 import styles from "./portfolio.module.css";
 
 function stocksOwned() {
-    const [topHoldings, setTopHoldings] = useState([
-        { symbol: 'AAPL', dayChangePercentage: 1.25, price: 172.45 },
-        { symbol: 'MSFT', dayChangePercentage: -0.78, price: 319.12 },
-        { symbol: 'TSLA', dayChangePercentage: 3.10, price: 245.88 },
-        { symbol: 'AMZN', dayChangePercentage: -2.15, price: 134.67 },
-        { symbol: 'GOOG', dayChangePercentage: 0.55, price: 122.34 },
-    ]);
+    const [portfolioValue, setPortfolioValue] = useState([1,374.85])
 
     return (
         <div className={styles.portfolio}> 
             <Navbar />
-            <h1>Welcome to Portfolio</h1>
+            <header>
+                <h1>Total Portfolio Value</h1>
+                <h2>{portfolioValue}</h2>
+            </header>
             
-                      {/*
-                        <section className={styles.topHoldings}>
-                            <p className={styles.topHoldingItem}>Top Holdings:</p>
-                            {topHoldings.map((stock, index) => (
-                                <div key={index} className={styles.topHoldingItem}>
-                                    <span className={styles.symbol}>{stock.symbol}</span>
-                                    <div className={styles.prices}>
-                                        <span className={styles.price}>${stock.price.toFixed(2)}</span>
-                                        <h4 className={portfolioDayChange >= 0 ? styles.positiveChange : styles.negativeChange}>
-                                            {portfolioDayChange >= 0 ? '+' : ''}
-                                            {portfolioDayChange.toFixed(2)}% today
-                                        </h4>
-                                    </div>
-                                </div>
-                            ))} 
-                        </section>
-                        */}
         </div>
         
     );
