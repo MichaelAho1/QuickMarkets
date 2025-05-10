c030e77b95d9414fb88b96ad39a612a9fd8d33a7
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-09 20:50:49 -0400

    added the rest of the tables to the db

--- server/simulator/admin.py
+++ server/simulator/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Stock
-from .models import ETF
+from .models import Stock, User, UserStock, StockPriceHistory, ETF
 
 admin.site.register(Stock)
+admin.site.register(User)
+admin.site.register(UserStock)
+admin.site.register(StockPriceHistory)
 admin.site.register(ETF)
--- server/simulator/models.py
+++ server/simulator/models.py
@@ -1,33 +1,54 @@
 from django.db import models
 
-class Stock(models.Model):
-    ticker = models.CharField(max_length=4)
-    name = models.CharField(max_length=25)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    prev_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    volatility = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
-    avgReturn = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
-    date = models.DateTimeField(auto_now_add=True)
-    #industries - Tech, Finance, Consumer, Energy
-    industry = models.CharField(max_length=15, default="Tech")
-    
-
+class User(models.Model):
+    username = models.CharField(max_length=50, primary_key=True)
+    cashBalance = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
-        return self.ticker
+        return self.username
     
-#Collection of stocks of a certain industry
 class ETF(models.Model):
-    ticker = models.CharField(max_length=4)
+    ticker = models.CharField(max_length=4, unique=True, primary_key=True) 
     name = models.CharField(max_length=25, default="")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     prev_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    volatility = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
-    avgReturn = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) #Percentage
+    volatility = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) 
+    avgReturn = models.DecimalField(max_digits=3, decimal_places=1, default=00.0) 
     date = models.DateTimeField(auto_now_add=True)
     #industries - Tech, Finance, Consumer, Energy
     industry = models.CharField(max_length=15, default="Tech")
 
 
+class Stock(models.Model):
+    ticker = models.CharField(max_length=10, primary_key=True)
+    stockName = models.CharField(max_length=50, default='') 
+    currPrice = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    sectorType = models.ForeignKey(ETF, on_delete=models.CASCADE, db_column='etf_ticker', to_field='ticker')
+    volatility = models.DecimalField(max_digits=3, decimal_places=1, default=0.0) 
+    avgReturn = models.DecimalField(max_digits=3, decimal_places=1, default=0.0) 
+
     def __str__(self):
         return self.ticker
+    
+class UserStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, db_column='username', to_field='username')
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, db_column='ticker', to_field='ticker')
+    sharesAmount = models.DecimalField(max_digits=10, decimal_places=4, default=0.0000) 
+    averageCost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        unique_together = (('user', 'stock'),)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.stock.ticker}"
+
+class StockPriceHistory(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, db_column='ticker', to_field='ticker')
+    date = models.DateField() 
+    closingPrice = models.DecimalField(max_digits=8, decimal_places=2, default=0.00) 
+
+    class Meta:
+        unique_together = (('stock', 'date'),)
+
+    def __str__(self):
+        return f"{self.stock.ticker} on {self.date}"
