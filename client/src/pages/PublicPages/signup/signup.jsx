16445f33e72f6dec55433d01200c9ea56d203713
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-09 22:12:24 -0400

    got rid of unessasary if statement

--- client/src/pages/PublicPages/signup/signup.jsx
+++ client/src/pages/PublicPages/signup/signup.jsx
@@ -23,13 +23,7 @@ function signup() {
 
         try {
             const res = await api.post(route + "/", { email, username, password })
-            if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/")
-            } else {
-                navigate("/login")
-            }
+            navigate("/login")
         } catch (error) {
             alert(error)
         } finally {
