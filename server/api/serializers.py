0ddffe30c3f22d88b4f4ef5cfd2cbcffc7bf3f5c
MichaelAho1 <ahoma@dukes.jmu.edu>
2025-05-10 12:04:36 -0400

    added a simulator user model

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User 
 from rest_framework import serializers
+from simulator.models import User as SimulatorUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,5 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data): 
         user = User.objects.create_user(**validated_data)
+        
+        SimulatorUser.objects.create(
+            username=user.username,
+            cashBalance=10000.00
+        )
 
         return user
